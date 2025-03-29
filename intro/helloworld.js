70bfed17aa00d96747489a0c13af5207f4b43576
Excorsite <panditsuyog4@gmail.com>
2025-03-29 12:16:44 +0545

    new array add

--- intro/helloworld.js
+++ intro/helloworld.js
@@ -68,3 +68,78 @@ let lastIndex2 = countries.length - 2;
 countries[lastIndex2] = "korea"; // replacing kenya by korea
 
 console.log(countries);
+
+//... Methods to manipulate array
+
+const arr = Array(); // creates an empty array
+console.log(arr);
+
+const eightXValues = Array(8); // it creates eight element values filled with 'X'
+console.log(eightXValues);
+
+const eight0calues = Array(8).fill(1); //it creates eight element values filled with '1'
+console.log(eight0calues);
+
+const four4Values = Array(4).fill(4);
+console.log(four4Values);
+
+//... Concatenating array using arrays...
+
+const firstList = [1, 2, 3];
+const secondList = [4, 5, 6];
+const thirdList = firstList.concat(secondList);
+
+console.log(thirdList);
+
+const fruits1 = ["banana", "orange", "mango", "lemon"];
+const vegatables = ["Tomato", "Potato", "Cabbage", "Onion", "Carrot"];
+const fruitsAndVegatables = fruits1.concat(vegatables); //concatenate the two aarays
+
+console.log(fruitsAndVegatables);
+
+// ... Getting index of an element in an array
+
+const numbers2 = [1, 2, 3, 4, 5];
+
+console.log(numbers2.indexOf(5)); // -> 4
+console.log(numbers2.indexOf(2)); // 1
+console.log(numbers2.indexOf(0)); // -1
+
+//... check an element if it extist in the array...
+
+const fruits2 = ["banana", "orange", "mango", "apple"];
+let index = fruits.indexOf("banana");
+
+if (index != -1) {
+  console.log("This fruit does exist in the array");
+} else {
+  console.log("This fruit does not exist in the array");
+}
+
+// we can use also ternary here
+index != -1
+  ? console.log("This fruit does exist in the array")
+  : console.log("This fruit does not exist in the array");
+
+//let us check if a avocado exist in the array
+let indexOfAvocado = fruits2.indexOf("avocado");
+if (indexOfAvocado != -1) {
+  console.log("This fruit does exist in the array");
+} else {
+  console.log("This fruit does not exist in the array");
+}
+
+// ... includes: to check if an item exist in an array.
+// If it exist it returns the true else it returns false.....
+
+const number = [1, 2, 3, 4, 5];
+
+console.log(number.includes(5));
+console.log(number.includes(0));
+console.log(number.includes(1));
+console.log(number.includes(6));
+
+const webTex = ["HTML", "CSS", "JavaScript", "React", "node"];
+
+console.log(webTex.includes("node"));
+console.log(webTex.includes("C"));

3f18736328efff9e0bcdfb55bd7b6e3b6ff8e50e
Excorsite <panditsuyog4@gmail.com>
2025-04-09 13:54:15 +0545

    Exercise

---
+++ intro/exercise.js
@@ -0,0 +1,10 @@
+// 1. Declare an empty array;
+const arr = [];
+console.log(arr);
+
+// Declare an array with more than 5 number of elements
+const arr1 = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr1);
+
+// Find the length of your array
+console.log("Length of array:", arr1.length);

8ab3a9441ddd6aa49688087de5e46597b0f8ce93
Excorsite <panditsuyog4@gmail.com>
2025-04-09 14:26:57 +0545

    push

--- intro/exercise.js
+++ intro/exercise.js
@@ -2,9 +2,36 @@
 const arr = [];
 console.log(arr);
 
-// Declare an array with more than 5 number of elements
+// 2. Declare an array with more than 5 number of elements
 const arr1 = [1, 2, 3, 4, 5, 6, 7];
 console.log(arr1);
 
-// Find the length of your array
+// 3. Find the length of your array
 console.log("Length of array:", arr1.length);
+
+// 4. Get the first item, the middle item and the last item of the array
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+console.log(webTechs[3]);
+console.log(webTechs[6]);
+console.log(webTechs[0]);
+
+// 5. Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+const mixedDataTypes = [
+  "variables",
+  "data",
+  "modules",
+  "indexOf",
+  "array",
+  "lastIndexOf",
+  "Splice",
+  "Slice",
+];
+console.log(mixedDataTypes.length);
